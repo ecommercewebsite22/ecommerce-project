acf5ebcac996847aec34a695af27f51d3edbeeb4
kwollner64 <110429380+kwollner64@users.noreply.github.com>
2022-09-19 18:33:13 -0500

    Update OrderItemsPojo.java

--- spring-boot-rest-data-ecommerce-maven/src/main/java/com/ecommerce/springbootrestdataecommercemaven/pojo/OrderItemsPojo.java
+++ spring-boot-rest-data-ecommerce-maven/src/main/java/com/ecommerce/springbootrestdataecommercemaven/pojo/OrderItemsPojo.java
@@ -2,14 +2,28 @@ package com.ecommerce.springbootrestdataecommercemaven.pojo;
 
 import javax.validation.constraints.NotNull;
 
-public class OrderItemsPojo {
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+@EqualsAndHashCode
+
+public class OrderItemPojo {
 	
 	private int itemId;
 	
 	@NotNull
-	private Integer orderNo, productSku, productQty;
+	private Integer suborderNo, orderNo, productSku, productQty;
 	@NotNull
-	Double productPrice;
+	private Double productPrice;
 	
 
 }
